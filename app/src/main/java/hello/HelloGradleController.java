d3c5336300fe2550890c1c96791aef92eda2fb84
Lars Wiedenhoeft <wiedenhoeft.lars@gmail.com>
2019-10-22 14:14:22 +0200

    migrate to gradle

---
+++ app/src/main/java/hello/HelloGradleController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloGradleController {
+
+    @GetMapping
+    public String helloGradle() {
+        return "Hello world! I am a Java application and was built with Gradle. ";
+    }
+
+}
